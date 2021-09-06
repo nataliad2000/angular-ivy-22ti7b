980e705938eee3156c050448efdc096bc16baa74
nataliad2000 <natalia.dominguez@alumnos.fesac.es>
2021-09-06 10:43:03 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule]
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductDetailsComponent },
+      { path: 'product-details/:productId', component: ProductDetailsComponent }
+    ])
+  ]
 })
 export class AppModule {}
